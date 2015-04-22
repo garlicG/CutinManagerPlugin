b18621c2e12865b0594b41885a101ddb68f608c3
garlicG <ggarlicG@gmail.com>
2015-04-22 18:42:21 +0900

    Add get intent method for custom extras on demo play.

--- cutin-support/src/main/java/com/garlicg/cutinsupport/Demo.java
+++ cutin-support/src/main/java/com/garlicg/cutinsupport/Demo.java
@@ -10,6 +10,7 @@ import android.content.Intent;
  * Call cutinservice.
  */
 public class Demo {
+
     private Intent mCutinIntent;
     private Context mContext;
     public Demo(Context context){
@@ -18,8 +19,6 @@ public class Demo {
 
     /**
      * Play cutin service. If there are playing cutin , stop it and new cutin service start.
-     * @param intent
-     * @return
      */
     public ComponentName play(Intent intent) {
         forceStop();
@@ -27,34 +26,38 @@ public class Demo {
         return mContext.startService(intent);
     }
 
-    public ComponentName play(Class<? extends Service> serviceClass) {
-        Intent intent = new Intent(mContext , serviceClass);
-        return play(intent);
-    }
-
     public ComponentName play(CutinItem item) {
-        return play(item.serviceClass, item.cutinId , item.triggerId);
-    }
-
-    public ComponentName play(Class<? extends Service> serviceClass , long cutinId , int triggerId) {
-        Intent intent = new Intent(mContext,serviceClass);
-        intent.putExtra(CutinManagerUtils.EXTRA_CUTIN_ID, cutinId);
-        intent.putExtra(CutinManagerUtils.EXTRA_TRIGGER_ID , triggerId);
-        return play(intent);
+        return play(getPlayIntent(item));
     }
 
     public ComponentName play(Class<? extends Service> serviceClass ,int triggerId){
-        Intent intent = new Intent(mContext,serviceClass);
-        intent.putExtra(CutinManagerUtils.EXTRA_TRIGGER_ID, triggerId);
-        return play(intent);
+        return play(serviceClass , triggerId);
     }
 
-    public ComponentName play(Class<? extends Service> serviceClass , String notifyPackageName ,String ticker){
-        Intent intent = new Intent(mContext,serviceClass);
-        intent.putExtra(CutinManagerUtils.EXTRA_TRIGGER_ID, CutinManagerUtils.TRIGGER_ID_NOTIFICATION);
+    public ComponentName play(Class<? extends Service> serviceClass , long cutinId , int triggerId) {
+        return play(getPlayIntent(serviceClass , cutinId , triggerId));
+    }
+
+    public ComponentName play(Class<? extends Service> serviceClass , long cutinId ,String notifyPackageName ,String ticker){
+        Intent intent = getPlayIntent(serviceClass , cutinId ,CutinManagerUtils.TRIGGER_ID_NOTIFICATION);
         intent.putExtra(CutinManagerUtils.EXTRA_NOTIFICATION_PACKAGE_NAME,notifyPackageName);
         intent.putExtra(CutinManagerUtils.EXTRA_NOTIFICATION_TICKER,ticker);
         return play(intent);
+    }
+
+    public Intent getPlayIntent(Class<? extends Service> cutinService , long cutinId){
+        return getPlayIntent(cutinService , cutinId , CutinManagerUtils.TRIGGER_ID_DEMO);
+    }
+
+    public Intent getPlayIntent(CutinItem item){
+        return getPlayIntent(item.serviceClass , item.cutinId , item.triggerId);
+    }
+
+    public Intent getPlayIntent(Class<? extends Service> cutinService , long cutinId , int triggerId){
+        Intent intent = new Intent(mContext ,cutinService );
+        intent.putExtra(CutinManagerUtils.EXTRA_CUTIN_ID, cutinId);
+        intent.putExtra(CutinManagerUtils.EXTRA_TRIGGER_ID , triggerId);
+        return intent;
     }
 
     /**
@@ -70,5 +73,12 @@ public class Demo {
         }
         mCutinIntent = null;
         return isStop;
+    }
+
+
+    @Deprecated
+    public ComponentName play(Class<? extends Service> serviceClass) {
+        Intent intent = new Intent(mContext , serviceClass);
+        return play(intent);
     }
 }
