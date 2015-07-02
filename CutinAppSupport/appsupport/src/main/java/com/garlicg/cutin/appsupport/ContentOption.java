53cee875b0334178fafb22c348aa074773654dc4
garlicG <ggarlicG@gmail.com>
2015-07-02 20:27:19 +0900

    Rename to TriggerInfo to ContentOption.

--- CutinAppSupport/appsupport/src/main/java/com/garlicg/cutin/appsupport/ContentOption.java
+++ CutinAppSupport/appsupport/src/main/java/com/garlicg/cutin/appsupport/ContentOption.java
@@ -2,14 +2,14 @@ package com.garlicg.cutin.appsupport;
 
 import android.content.Intent;
 
-public class TriggerInfo {
+public class ContentOption {
 
     /**
      * Trigger type
      *
      * @see com.garlicg.cutin.appsupport.ManagerUtils#EXTRA_TRIGGER_TYPE
      */
-    public final int type;
+    public final int triggerType;
 
     /**
      * TITLE of useful content for CUT-IN appearance from Trigger
@@ -21,31 +21,31 @@ public class TriggerInfo {
      */
     public final String contentMessage;
 
-    private TriggerInfo(int triggerType , String contentTitle, String contentMessage){
-        this.type = triggerType;
+    private ContentOption(int triggerType, String contentTitle, String contentMessage){
+        this.triggerType = triggerType;
         this.contentTitle = contentTitle;
         this.contentMessage = contentMessage;
     }
 
-    public static TriggerInfo from(Intent intent){
-        return new TriggerInfo(
+    public static ContentOption from(Intent intent){
+        return new ContentOption(
                 intent.getIntExtra(ManagerUtils.EXTRA_TRIGGER_TYPE , ManagerUtils.TRIGGER_TYPE_DEMO),
                 intent.getStringExtra(ManagerUtils.EXTRA_CONTENT_TITLE) ,
                 intent.getStringExtra(ManagerUtils.EXTRA_CONTENT_MESSAGE));
     }
 
-    public static TriggerInfo emulate(String contentTitle , String contentMessage , int type){
-        return new TriggerInfo(
+    public static ContentOption emulate(String contentTitle , String contentMessage , int type){
+        return new ContentOption(
                 type,
                 contentTitle,
                 contentMessage);
     }
 
-    public static TriggerInfo emulate(String contentTitle , String contentMessage){
+    public static ContentOption emulate(String contentTitle , String contentMessage){
         return emulate(contentTitle , contentMessage , ManagerUtils.TRIGGER_TYPE_DEMO);
     }
 
-    public static TriggerInfo emulate(int type){
+    public static ContentOption emulate(int type){
         return emulate("" , "" , type);
     }
 
